067d611569453a477766c583c496e25044a54e03
Marvin Ren <marvin.ren@yale.edu>
2021-09-14 00:57:01 -0400

    sortable

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,3 +1,21 @@
+$(".card .list-group").sortable({
+  connectWith: $(".card .list-group"),
+  scroll: false,
+  tolerance: "pointer",
+  helper: "clone",
+  activate: function(event) {
+    console.log("activate", this);
+  },
+  deactivate: function(event) {
+    console.log("deactivate", this);
+  },
+  over: function(event) {
+    console.log("over", event.target);
+  },
+  update: function(event) {
+    console.log("update", this);
+  }
+});
 var tasks = {};
 
 var createTask = function(taskText, taskDate, taskList) {
